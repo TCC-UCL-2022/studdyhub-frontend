474a9c59af2c63734255129081e8a1244beda1e1
Felipe Ferrarini <ferrarinifelipe@gmail.com>
2022-05-25 20:53:19 -0300

    Improve radio card styles

--- src/features/ui/forms/radio-card.tsx
+++ src/features/ui/forms/radio-card.tsx
@@ -1,33 +1,49 @@
-import { Box, useRadio, UseRadioProps } from "@chakra-ui/react";
+import {
+  Box,
+  ButtonProps,
+  Center,
+  useColorModeValue,
+  useRadio,
+  UseRadioProps,
+} from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 
 type RadioCardProps = UseRadioProps & {
   fullWidth?: boolean;
+  colorScheme?: ButtonProps["colorScheme"];
 };
 
 export const RadioCard = ({
   children,
   fullWidth,
+  colorScheme = "blue",
   ...props
 }: PropsWithChildren<RadioCardProps>) => {
   const { getInputProps, getCheckboxProps } = useRadio(props);
 
   const input = getInputProps();
   const checkbox = getCheckboxProps();
+  const bgColor = useColorModeValue("gray.100", "gray.700");
+  const activeBgColor = useColorModeValue(
+    `${colorScheme}.500`,
+    `${colorScheme}.200`
+  );
 
   return (
     <Box as="label" w={fullWidth ? "100%" : ""}>
       <input {...input} />
-      <Box
+      <Center
         {...checkbox}
         cursor="pointer"
         borderWidth="1px"
         borderRadius="md"
         boxShadow="md"
+        bg={bgColor}
+        borderColor={bgColor}
         _checked={{
-          bg: "blue.600",
-          color: "white",
-          borderColor: "blue.600",
+          bg: activeBgColor,
+          color: useColorModeValue("white", "black"),
+          borderColor: activeBgColor,
         }}
         _focus={{
           boxShadow: "outline",
@@ -35,9 +51,12 @@ export const RadioCard = ({
         px={5}
         py={3}
         transition="all 0.2s"
+        minH="28"
+        fontWeight="semibold"
+        fontSize="lg"
       >
         {children}
-      </Box>
+      </Center>
     </Box>
   );
 };
