e5c0f285d1462ae546c5f468ab24d8e9f78f94aa
Felipe Ferrarini <ferrarinifelipe@gmail.com>
2022-06-05 16:47:18 -0300

    Add video quality selector to the video player

--- src/features/ui/media/video-player/video-player.tsx
+++ src/features/ui/media/video-player/video-player.tsx
@@ -1,6 +1,13 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import videojs, { VideoJsPlayer, VideoJsPlayerOptions } from "video.js";
 import "video.js/dist/video-js.css";
+
+// Lib to handle quality resolutions
+import videoQualitySelector from "@silvermine/videojs-quality-selector";
+import "@silvermine/videojs-quality-selector/dist/css/quality-selector.css";
+
+// Bind the videojs library to the window object
+videoQualitySelector(videojs);
 
 type VideoPlayerProps = {
   options: VideoJsPlayerOptions;
@@ -16,16 +23,7 @@ export const VideoPlayer = ({
   const videoRef = useRef<HTMLVideoElement>(null);
   const playerRef = useRef<VideoJsPlayer | null>(null);
 
-  const [options2] = useState<VideoJsPlayerOptions>({
-    controls: true,
-    autoplay: false,
-    fill: true,
-    preload: "auto",
-    playbackRates: [0.5, 1, 1.5, 2],
-    ...options,
-  });
-
-  useEffect(() => {
+  const createVideoPlayerJs = useCallback(() => {
     if (!playerRef.current) {
       const videoElement = videoRef.current;
 
@@ -33,19 +31,44 @@ export const VideoPlayer = ({
 
       const player = (playerRef.current = videojs(
         videoElement,
-        options2,
+        {
+          controls: true,
+          autoplay: false,
+          fill: true,
+          preload: "auto",
+          playbackRates: [0.5, 1, 1.5, 2],
+          ...options,
+        },
         () => {
-          videojs.log("player is ready");
+          videojs.log("Player is ready");
           onReady?.(player);
         }
       ));
 
+      player.controlBar.addChild("QualitySelector");
+
       player.on("ended", () => {
+        videojs.log("Video has ended");
         onEnded?.();
-        alert("Video has ended");
       });
     }
-  }, [onEnded, onReady, options, options2, videoRef]);
+  }, [onEnded, onReady, options, videoRef]);
+
+  useEffect(() => {
+    createVideoPlayerJs();
+  }, [createVideoPlayerJs]);
+
+  const handleUpdateSources = useCallback(() => {
+    if (playerRef.current && options.sources) {
+      const player = playerRef.current;
+
+      player.src(options.sources);
+    }
+  }, [options]);
+
+  useEffect(() => {
+    handleUpdateSources();
+  }, [handleUpdateSources]);
 
   return (
     <div data-vjs-player>
